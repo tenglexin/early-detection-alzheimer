5a3026dba52c1d77e47880005fbff5326cdc9aa6
txin <txin@lenovo.com>
2025-12-09 00:31:42 +0800

    Added Streamlit app and requirements

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Early Detection of Alzheimer’s Disease")
+st.write("This is a basic Streamlit app. Your ML model will go here.")
+
+if st.button("Click Me"):
+    st.success("Streamlit App Working!")
